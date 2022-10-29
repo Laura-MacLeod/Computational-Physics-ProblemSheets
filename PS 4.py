0bb34be431bc675e49c16609a90d4c4f465466d7
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-10-29 17:06:00 +0100

    Update PS 4.py

--- PS 4.py
+++ PS 4.py
@@ -6,7 +6,7 @@ Created on Wed Oct 26 08:59:53 2022
 @author: laura
 """
 
-# ------- PROBLEM SHEET 3 --------
+# ------- PROBLEM SHEET 4 --------
 
 
 import matplotlib.pyplot as plt
@@ -16,3 +16,48 @@ import pandas as pd
 
 
 #%% ------- QUESTION 1 --------
+
+
+def LinCong(n, a, b, x0):
+    randoms = []
+    x = x0
+    for i in range(n):
+        x = (a * x) % b
+        randoms.append(x)
+    print(randoms)
+    return randoms
+
+LinCong(15, 7, 11, 1)
+
+'''
+Only 10 unique numbers can be generated due to the use of the 
+With a=7, b=12 and x0=1, the numbers are just 7s and 1s, which are obviously not random
+Can't seem to fix this by changing a and x0 individually, but changing both of them seems to fix it
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
